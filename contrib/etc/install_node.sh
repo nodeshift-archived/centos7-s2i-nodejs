d3920626834f77a7efee2fe369c4a5844a4798d2
Lance Ball <lball@redhat.com>
2018-02-26 09:32:10 -0500

    feat: install dev dependencies only in DEBUG mode (#36)
    Connects to: https://github.com/bucharest-gold/centos7-s2i-nodejs/issues/34
    
    This change also ensures that symlinks in node_modules/.bin are preserved

--- contrib/etc/install_node.sh
+++ contrib/etc/install_node.sh
@@ -15,7 +15,7 @@ chown -R 1001:0 /opt/app-root
 chmod -R ug+rwx /opt/app-root
 
 # Fix permissions for the npm update-notifier
-# chmod -R 777 /opt/app-root/src/.config
+chmod -R 777 /opt/app-root/src/.config
 
 # Delete NPM things that we don't really need (like tests) from node_modules
 find /usr/local/lib/node_modules/npm -name test -o -name .bin -type d | xargs rm -rf
