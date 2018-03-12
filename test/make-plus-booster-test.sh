df9d5a5138776d2567ba18cf6f9f7a9ecd3ef579
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-03-12 06:26:56 +0100

    squash: move script to test directory

--- test/make-plus-booster-test.sh
+++ test/make-plus-booster-test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-node_version=$(cat versions.mk|grep NODE_VERSION|cut -d'=' -f2)
-npm_version=$(cat versions.mk|grep NPM_VERSION|cut -d'=' -f2)
+node_version=$(cat ../versions.mk|grep NODE_VERSION|cut -d'=' -f2)
+npm_version=$(cat ../versions.mk|grep NPM_VERSION|cut -d'=' -f2)
 image_name=rhoar/local-centos-nodejs-s2i
 booster_image_name=nodejs-rest-http-s2i-rhoar
 
-make
+make -C ..
 
 # Build the docker image
-docker build -t $image_name --build-arg NODE_VERSION=$node_version --build-arg NPM_VERSION=$npm_version .
+docker build -t $image_name --build-arg NODE_VERSION=$node_version --build-arg NPM_VERSION=$npm_version ../.
 
-pushd ../nodejs-rest-http/
+pushd ../../nodejs-rest-http/
 
 s2i build . ${image_name}:latest $booster_image_name --pull-policy never
 
