3019688f2e9facdb089ac300a919fa07237e1a94
Helio Frota <00hf11@gmail.com>
2018-11-05 16:41:02 -0300

    chore: update to use nodeshift org

--- contrib/etc/install_node.sh
+++ contrib/etc/install_node.sh
@@ -17,8 +17,8 @@ git config --system url."https://".insteadOf git://
 git config --system url."https://".insteadOf ssh://
 git config --list
 
-yum install -y https://github.com/bucharest-gold/node-rpm/releases/download/v${NODE_VERSION}/rhoar-nodejs-${NODE_VERSION}-1.el7.centos.x86_64.rpm
-yum install -y https://github.com/bucharest-gold/node-rpm/releases/download/v${NODE_VERSION}/npm-${NPM_VERSION}-1.${NODE_VERSION}.1.el7.centos.x86_64.rpm
+yum install -y https://github.com/nodeshift/node-rpm/releases/download/v${NODE_VERSION}/rhoar-nodejs-${NODE_VERSION}-1.el7.centos.x86_64.rpm
+yum install -y https://github.com/nodeshift/node-rpm/releases/download/v${NODE_VERSION}/npm-${NPM_VERSION}-1.${NODE_VERSION}.1.el7.centos.x86_64.rpm
 
 rpm -V $INSTALL_PKGS
 yum clean all -y
