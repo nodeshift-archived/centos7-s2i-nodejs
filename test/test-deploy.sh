4772f8c7ddd0fdfaffa4fa168114f553527fe75e
Lucas Holmquist <lholmqui@redhat.com>
2019-05-01 08:35:58 -0400

    chore: update tests for node 12.x

--- test/test-deploy.sh
+++ test/test-deploy.sh
@@ -8,13 +8,13 @@ docker login -u `oc whoami` -p `oc whoami -t` 172.30.1.1:5000
 oc new-project nodeshift
 
 # Creates a tag on openshift docker registry based on the local created image.
-docker tag nodeshift/centos7-s2i-nodejs:11.x 172.30.1.1:5000/nodeshift/centos7-s2i-nodejs:11.x
+docker tag nodeshift/centos7-s2i-nodejs:12.x 172.30.1.1:5000/nodeshift/centos7-s2i-nodejs:12.x
 
 # Pushes the image to the openshift docker registry.
-docker push 172.30.1.1:5000/nodeshift/centos7-s2i-nodejs:11.x
+docker push 172.30.1.1:5000/nodeshift/centos7-s2i-nodejs:12.x
 
 # Creates a new app based on the pushed image.
-oc new-app 172.30.1.1:5000/nodeshift/centos7-s2i-nodejs:11.x~https://github.com/nodeshift/nodejs-rest-http
+oc new-app 172.30.1.1:5000/nodeshift/centos7-s2i-nodejs:12.x~https://github.com/nodeshift/nodejs-rest-http
 
 # Exposes to confirm that the app was successfully deployed.
 timeout 1m oc expose svc/nodejs-rest-http
